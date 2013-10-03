237d78f52141b123b7edd91243bcd14c88c83f2b
Meredith Bryan <merbryan@gmail.com>
2013-10-03 14:48:27 -0700

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,136 @@
+
+// /*function navCells()
+// */ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var cellArray = [["","",""],["","",""],["","",""]];
+	
+
+
+// var turn =1;
+
+// function playBox()
+// {
+
+// if(event.target.innerHTML == "")  {
+
+// 		switch(turn) {
+// 		case 1:
+// 		event.target.innerHTML = "X";
+// 		event.target.value = 1;
+// 		break;
+// 		case 2:
+// 		event.target.innerHTML = "<img src='100x100cat.png' />";
+// 		event.target.value = -1;
+// 		break;
+// 		case 3:
+// 		event.target.innerHTML = "X";
+// 		event.target.value = 1;
+// 		break;
+// 		case 4:
+// 		event.target.innerHTML = "<img src='100x100cat.png' />";
+// 		event.target.value = -1;
+// 		break;
+// 		case 5:
+// 		event.target.innerHTML = "X";
+// 		event.target.value = 1;
+// 		break;
+// 		case 6:
+// 		event.target.innerHTML = "<img src='100x100cat.png' />";
+// 		event.target.value = -1;
+// 		break;
+// 		case 7:
+// 		event.target.innerHTML = "X";
+// 		event.target.value = 1;
+// 		break;
+// 		case 8:
+// 		event.target.innerHTML = "<img src='100x100cat.png' />";
+// 		event.target.value = -1;
+// 		break;
+// 		case 9:
+// 		event.target.innerHTML = "X";
+// 		event.target.value = 1;
+// 		break;
+// 		} 
+// 		turn = turn+1;
+
+// 	} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(r=0;r<=2;++r)
+// {	
+// 	for(c=0;c<=2;++c)
+// {
+// 		cellArray[r][c] =
+// 			document.getElementById("box"+(r+1)+"_"+(c+1)).innerHTML;
+//  		}
+
+//  	}
+
+
+//  	for(c=0;c<=2;++c)
+
+
+// {
+
+
+//  	if (cellArray[0][c] == cellArray[1][c] && 
+//  		cellArray[1][c] == cellArray[2][c] && 
+//  		cellArray[0][c] != "")
+//  		alert(cellArray[0][c] +"won in column" + c);
+
+
+//  	if (cellArray[c][0] == cellArray[c][1] && 
+//  		cellArray[c][1] == cellArray[c][2] && 
+//  		cellArray[c][0] != "")
+//  		alert(cellArray[c][0] +"won in row" + c);
+
+
+//  }
+
+// }
+
