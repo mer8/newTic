1a87a0ac5597b29314d254663a09b3be70396dbb
Meredith Bryan <merbryan@gmail.com>
2013-10-04 14:25:02 -0700

    Have now switched to object cells and am now having click issues

--- app/scripts/controllers/TicTacCtrl.js
+++ app/scripts/controllers/TicTacCtrl.js
@@ -1,69 +1,74 @@
 function TicTacCtrl($scope){
 	
-$scope.repeatTicBoard = [['','',''],['','',''],['','','']];
+$scope.repeatTicBoard = [[{value:''}, {value:''}, {value:''}],
+  [{value:''}, {value:''}, {value:''}],
+  [{value:''}, {value:''}, {value:''}]];
 
 
 var playerTurn = 1;
 
-$scope.clickSquare = function(row,column) {
-		
+  $scope.clickSquare = function(cell) {
+	
+
 		if(playerTurn % 2 == 1) 
-		// {
-			event.target.innerHTML = "<img src='../../images/cat2pic.gif' />";
-		// 	$scope.repeatTicBoard[row][column] = "Purple Cat"
-		// }
+			cell.value = "<img src='../../images/cat2pic.gif' />";
 		else 
-		// {
-			event.target.innerHTML = "<img src='../../images/typingcat.gif' />";
-		// 	$scope.repeatTicBoard[row][column] = "White Cat"
-		// }
+			cell.value = "<img src='../../images/typingcat.gif' />";
+
 		++playerTurn;
 
 
 
+
+
+// if(this.repeatTicBoard[0][0] == "<img src='../../images/cat2pic.gif' />" &&
+// 	this.repeatTicBoard[0][1]
+
+
+
 	 
-for(c=0;c<=2;++c) {
+// for(c=0;c<=2;++c) {
 
 
-// **This is for row and column wins**
+// // **This is for row and column wins**
 
- 	if (this.repeatTicBoard[0][c] == this.repeatTicBoard[1][c] && 
- 		this.repeatTicBoard[1][c] == this.repeatTicBoard[2][c] && 
- 		this.repeatTicBoard[0][c] != "")
- 		{
- 			alert(this.repeatTicBoard[0][c] +" won in column " + c);
- 		location.reload();
- 		}
-
-
- 	if (this.repeatTicBoard[c][0] == this.repeatTicBoard[c][1] && 
- 		this.repeatTicBoard[c][1] == this.repeatTicBoard[c][2] && 
- 		this.repeatTicBoard[c][0] != "")
- 		{
- 			alert(this.repeatTicBoard[c][0] +" won in row " + c);
- 		location.reload();
- 		}
+//  	if (this.repeatTicBoard[0][c] == this.repeatTicBoard[1][c] && 
+//  		this.repeatTicBoard[1][c] == this.repeatTicBoard[2][c] && 
+//  		this.repeatTicBoard[0][c] != "")
+//  		{
+//  			alert(this.repeatTicBoard[0][c] +" won in column " + c);
+//  		location.reload();
+//  		}
 
 
- }
+//  	if (this.repeatTicBoard[c][0] == this.repeatTicBoard[c][1] && 
+//  		this.repeatTicBoard[c][1] == this.repeatTicBoard[c][2] && 
+//  		this.repeatTicBoard[c][0] != "")
+//  		{
+//  			alert(this.repeatTicBoard[c][0] +" won in row " + c);
+//  		location.reload();
+//  		}
 
- // **This is for diagonal wins**
 
-  	if (this.repeatTicBoard[0][0] == this.repeatTicBoard[1][1] &&
- 		this.repeatTicBoard[1][1] == this.repeatTicBoard[2][2] &&
- 		this.repeatTicBoard[2][2] != "")
- 		{
- 			alert(this.repeatTicBoard[2][2] +" won in diag " + c);
- 		location.reload();
- 		}
+//  }
 
- 	if (this.repeatTicBoard[0][2] == this.repeatTicBoard[1][1] &&
- 		this.repeatTicBoard[1][1] == this.repeatTicBoard[2][0] &&
- 		this.repeatTicBoard[2][0] != "")
- 		{
- 			alert(this.repeatTicBoard[2][0] +" won in diag " + c);
- 		location.reload();
- 		}
+//  // **This is for diagonal wins**
+
+//   	if (this.repeatTicBoard[0][0] == this.repeatTicBoard[1][1] &&
+//  		this.repeatTicBoard[1][1] == this.repeatTicBoard[2][2] &&
+//  		this.repeatTicBoard[2][2] != "")
+//  		{
+//  			alert(this.repeatTicBoard[2][2] +" won in diag " + c);
+//  		location.reload();
+//  		}
+
+//  	if (this.repeatTicBoard[0][2] == this.repeatTicBoard[1][1] &&
+//  		this.repeatTicBoard[1][1] == this.repeatTicBoard[2][0] &&
+//  		this.repeatTicBoard[2][0] != "")
+//  		{
+//  			alert(this.repeatTicBoard[2][0] +" won in diag " + c);
+//  		location.reload();
+//  		}
 
 
 
