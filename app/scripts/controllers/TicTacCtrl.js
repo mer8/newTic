5d2e46b04ff1e8ffe91922ad3cb138273d24e385
Meredith Bryan <merbryan@gmail.com>
2013-10-11 11:27:44 -0700

    creates many games

--- app/scripts/controllers/TicTacCtrl.js
+++ app/scripts/controllers/TicTacCtrl.js
@@ -1,98 +1,98 @@
 'use strict';
 
 angular.module('newTicApp')
-  .controller('TicTacCtrl', ['$scope', 'angularFire', function ($scope, angularFire) {
-
-// function TicTacCtrl($scope, angularFire){
+  .controller('TicTacCtrl', ['$scope', 'angularFire', function ($scope, angularFire) { //1
 
 
-// $scope.repeatTicBoard = [[{value:''}, {value:''}, {value:''}],
-//   [{value:''}, {value:''}, {value:''}],
-//   [{value:''}, {value:''}, {value:''}]];
-
-  $scope.winmessage = "";
-
-	var database = new Firebase("https://my-maintic-mer8.firebaseio.com/room/");
-	// BINDING TO FIREBASE
-	//$scope.games = [];
-	//$scope.queue = {};
+  	$scope.games = [];
+	$scope.room= {};
+  	$scope.winmessage = "";
 
 
+  	var games = new Firebase("https://my-maintic-mer8.firebaseio.com/games");
+    angularFire(games, $scope, "games").then(function () { //2
 
-	$scope.room={};
-
-//var games = new Firebase("https://my-maintic-mer8.firebaseio.com/games");
-//angularFire(games, $scope, "games").then(function () {  
-
-
-//var queue = new Firebase("https://my-maintic-mer8.firebaseio.com/queue");
-//angularFire(queue, $scope, "games").then(function () {    blah blah all that stuff like before.... end paren at end ***move down***
+      var room = new Firebase("https://my-maintic-mer8.firebaseio.com/room");
+      angularFire(room, $scope, "room").then(function () { //3.1
 
 
-	var promise = angularFire(database, $scope, "room");
+      	if ($scope.room.gameId == undefined) { 
+      		console.log("I'm player 1");
+      		$scope.player = "p1";
 
-promise.then( function() {
-// ***pick up here*** 
-//if ($scope.room.gameId == undefined) {
-	//console.log("I'm player 1");
-	//$scope.player = "p1";
-	//var newGame = {
-		//board: ["","",""],
-		//turn: 'p1',
-		//win: false'
-		//turnCount:0
-//};
+          var newGame = {
+            board: [[{value:''}, {value:''}, {value:''}],
+	  [{value:''}, {value:''}, {value:''}],
+	  [{value:''}, {value:''}, {value:''}]],
+            turn: 'p1',
+            waiting: true,
+            win: false,
+            turnCount: 0
+          };
 
-//$scope.gameID = $scope.games.push(newGame) - 1;
-//$scope.queue.gameId = $scope.gameId;
-//console.log("Player 1's game is: " + $scope.gameId);
+          $scope.gameId = $scope.games.push(newGame) - 1;
 
-//CREATE GAME
+          $scope.room.gameId = $scope.gameId;          
+          console.log("Player 1's game is: " + $scope.gameId);
+      } else {
+      	 console.log("I'm player 2");
+      	 $scope.player = "p2";
+      	 $scope.gameId = $scope.room.gameId;
+      	 $scope.room = {};
+      	 console.log("Player 2's game is: " + $scope.gameId);
+
+      }
 
 
 
-//} else {
-		//console.log("I'm player 2");
-		//$scope.player = "p2";
 
-//$scope.gameId = $scope.queue.gameId;
 
-		
 
-//READ GAME ID FROM QUEUE
 
-//$scope.queue = {};
-//console.log
-//}
-//});
 
-	$scope.room={
+
+
+
+	$scope.room = 
+	{
 	repeatTicBoard: [[{value:''}, {value:''}, {value:''}],
 	  [{value:''}, {value:''}, {value:''}],
 	  [{value:''}, {value:''}, {value:''}]],
 	  playerTurn: 1,
 	  hasWon: false,
-};
-
-
-
-  // angularFire(database, $scope, "repeatTicBoard");
-//you need to add "games" into where "repeatTicBoard" and then {an object, which is your game*}
-
-  $scope.findImg = function(cell) { 
-		
-		switch(cell.value)
-		{
-			case "X":
-			return "../../images/cat2pic.gif";
-			case "O":
-			return "../../images/typingcat.gif";
-			case "":
-			return "../../images/1x1.png";
-		}
 	};
 
-	$scope.clickSquare = function(cell) {
+
+
+  
+
+
+
+
+
+
+  
+		 $scope.findImg = function(cell) 
+		  { 
+				switch(cell.value)
+				{
+					case "X":
+					return "../../images/cat2pic.gif";
+					case "O":
+					return "../../images/typingcat.gif";
+					case "":
+					return "../../images/1x1.png";
+				}
+			};
+
+
+
+
+
+
+
+
+	$scope.clickSquare = function(cell) { //3
 		if(cell.value != "")
 			return;
 
@@ -176,78 +176,12 @@ var hasWon = false;
 
 
 
-	}
+	} //3
 
+ }); //3.1
 
+}); //2
 
-});
-
-}]);
-
-
-
-
-
-// if(this.repeatTicBoard[0][0] == "<img src='../../images/cat2pic.gif' />" &&
-// 	this.repeatTicBoard[0][1]
-
-
-
-	 
-// for(c=0;c<=2;++c) {
-
-
-// // **This is for row and column wins**
-
-//  	if (repeatTicBoard.value[0][c] == this.repeatTicBoard[1][c] && 
-//  		this.repeatTicBoard[1][c] == this.repeatTicBoard[2][c] && 
-//  		this.repeatTicBoard[0][c] != "")
-//  		{
-//  			alert(this.repeatTicBoard[0][c] +" won in column " + c);
-//  		location.reload();
-//  		}
-
-
-//  	if (this.repeatTicBoard[c][0] == this.repeatTicBoard[c][1] && 
-//  		this.repeatTicBoard[c][1] == this.repeatTicBoard[c][2] && 
-//  		this.repeatTicBoard[c][0] != "")
-//  		{
-//  			alert(this.repeatTicBoard[c][0] +" won in row " + c);
-//  		location.reload();
-//  		}
-
-
-//  }
-
-//  // **This is for diagonal wins**
-
-//   	if (this.repeatTicBoard[0][0] == this.repeatTicBoard[1][1] &&
-//  		this.repeatTicBoard[1][1] == this.repeatTicBoard[2][2] &&
-//  		this.repeatTicBoard[2][2] != "")
-//  		{
-//  			alert(this.repeatTicBoard[2][2] +" won in diag " + c);
-//  		location.reload();
-//  		}
-
-//  	if (this.repeatTicBoard[0][2] == this.repeatTicBoard[1][1] &&
-//  		this.repeatTicBoard[1][1] == this.repeatTicBoard[2][0] &&
-//  		this.repeatTicBoard[2][0] != "")
-//  		{
-//  			alert(this.repeatTicBoard[2][0] +" won in diag " + c);
-//  		location.reload();
-//  		}
-
-
-
-
-
-
-
-
-
-
-
-
-
+}]); //1
 
 
